4f5f92680730dcdac4b7cf8f5437200e2643dab2
Geno Lamb <eablamb@gmail.com>
2017-10-21 12:59:38 -0400

    Add list-filter component
    The list-filter component provides a search bar to filter rental results.

---
+++ app/components/list-filter.js
@@ -0,0 +1,26 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['list-filter'],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    // seed initial entries by calling filter with an empty value
+    this.get('filter')('').then((results) => this.set('results', results));
+  },
+
+  actions: {
+    // Calls 'filter' on the value set in the 'value' attribute of the input helper
+    handleFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+      // `then' expects `filterAction' to return a promise.  A promise is a JS object
+      // that represents the result of an asynchronous function.  Since filterAction
+      // may not return a result immediately, `then' will wait until it receives the result
+      // to execute its code.
+      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+    }
+  }
+
+});
